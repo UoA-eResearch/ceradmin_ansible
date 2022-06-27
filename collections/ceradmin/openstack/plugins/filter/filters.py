4a762a573328871bb3ec3ba8965038879a13c65f
Martin Feller <m.feller@auckland.ac.nz>
2022-06-27 13:05:23 +1200

    include floating ips

--- collections/ceradmin/openstack/plugins/filter/filters.py
+++ collections/ceradmin/openstack/plugins/filter/filters.py
@@ -1,21 +1,31 @@
 import re
 
 
-def get_server_ipv4s(openstack_instance: dict, ip_regex: str = None):
+def get_server_ipv4s(server_dict: dict, ip_regex: str = None):
     ips = []
-    networks = openstack_instance['addresses'].values()
+    # standard IPv4 addresses
+    networks = server_dict['addresses'].values()
     for network in networks:
         if ip_regex is None:
             ips.extend((address['addr'] for address in network if address['version'] == 4))
         else:
             ips.extend((address['addr'] for address in network
                        if address['version'] == 4 and re.match(ip_regex, address['addr'])))
-    return ips
+
+    # floating ips
+    if 'floating_ips' in server_dict:
+        if ip_regex is None:
+            ips.extend((fip['floating_ip_address'] for fip in server_dict['floating_ips']))
+        else:
+            ips.extend((fip['floating_ip_address'] for fip in server_dict['floating_ips']
+                        if re.match(ip_regex, fip['floating_ip_address'])))
+
+    return list(set(ips))
 
 
-def get_first_server_ipv4(openstack_instance: dict, ip_regex: str):
+def get_first_server_ipv4(server_dict: dict, ip_regex: str):
     ip = None
-    ips = get_server_ipv4s(openstack_instance, ip_regex)
+    ips = get_server_ipv4s(server_dict, ip_regex)
     if len(ips) > 0:
         ip = ips[0]
     return ip
